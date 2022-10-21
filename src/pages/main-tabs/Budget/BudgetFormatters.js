7c45fbe4c1d885e03a631e12e8cdc29480d519e9
DineshB <dineshba03@gmail.com>
2022-10-21 10:30:16 -0500

    removed unused items and declarations

--- src/pages/main-tabs/Budget/BudgetFormatters.js
+++ src/pages/main-tabs/Budget/BudgetFormatters.js
@@ -1,27 +1,7 @@
 import React from 'react';
-import {styled} from "@mui/material/styles";
-import {
-    TableCell,
-    tableCellClasses,
-    TableRow,
-} from "@mui/material";
 import './Budget.css';
 
-
-export const StyledTableCell = styled(TableCell)(({theme}) => ({
-    [`&.${tableCellClasses.head}`]: {
-        backgroundColor: "#191c1e", color: theme.palette.common.white,
-    }, [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-    },
-}));
-export const StyledTableRow = styled(TableRow)(() => ({
-    '&:nth-of-type(odd)': {}, // hide last border
-    '&:last-child td, &:last-child th': {
-        border: 0,
-    },
-}));
-
+//todo: remove this js file and move the formatter to a higher hierarchy in src
 export const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
