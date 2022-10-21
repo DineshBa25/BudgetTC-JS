bb1b6084301c251f6c50eafdeee4d291d8872f9a
DineshB <dineshba03@gmail.com>
2022-10-21 10:19:27 -0500

    add minor bug fixes and added allocated expenses info overlays along with onClick action for edit icon button that triggers edit item modal.

--- src/pages/main-tabs/Budget/BudgetCategoryItem.js
+++ src/pages/main-tabs/Budget/BudgetCategoryItem.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {
-    Avatar,
     Button,
-    ButtonToolbar,
     Form,
     IconButton,
     Input,
@@ -26,7 +24,6 @@ import BudgetExpenseItemTransferList from "./BudgetExpenseItemTransferList"
 class BudgetCategoryItem extends React.Component {
     constructor(props) {
         super(props);
-        //console.log("Item Unallocated Expenses: ", this.props.unallocatedExpenses);
         this.state = {
             item: props.item,
             name: props.item.name,
@@ -43,34 +40,30 @@ class BudgetCategoryItem extends React.Component {
                 expenseList: props.item.expenseList,
             },
             showNameInput: false,
+            showAmountInput: false,
             expensesAllocated: 0,
         }
     }
 
     componentDidMount() {
         let total = 0;
-        console.log("Expense List: ", this.state.expenseList);
         Object.entries(this.state.expenseList).forEach((expense) => {
             if (expense[1].amount !== undefined) {
                 total += expense[1].amount;
             }
         })
-        console.log("Total: ", total);
         //do not end until state udpate was completed
-        this.setState({expensesAllocated: total}, () => {
-            console.log("Expenses Allocated: ", this.state.expensesAllocated);
-        });
+        this.setState({expensesAllocated: total});
     }
+
 
     getExpensesAllocated() {
         let total = 0;
-        console.log("Expense List: ", this.state.expenseList);
         Object.entries(this.state.expenseList).forEach((expense) => {
             if (expense[1].amount !== undefined) {
                 total += expense[1].amount;
             }
         })
-        console.log("Total: ", total);
         return total;
     }
 
@@ -83,9 +76,7 @@ class BudgetCategoryItem extends React.Component {
 
     toObject(arr) {
         let rv = {};
-
         for (let i = 0; i < arr.length; ++i){
-            console.log(arr[i][1]);
             rv["item"+(i+1)] = arr[i][1];
         }
         return rv;
@@ -93,28 +84,21 @@ class BudgetCategoryItem extends React.Component {
     //modifies the expenselist. changes is a {} of the form {addedToLeft: [], removedFromLeft: [], addedToRight: [], removedFromRight: []}
     handleExpenseListChange = (changes) => {
 
-        console.log("Current Expense List: ", this.state.expenseList);
-
         let newExpenseList = this.toObject(changes.right);
-
-        console.log(changes)
-
-        console.log("New expense list: ", newExpenseList);
 
         this.setState({expenseList: newExpenseList}, () => {
            this.setState({expensesAllocated: this.getExpensesAllocated()});
         });
 
-        //update the unallocated expenses in the parent function
-        console.log(changes)
         this.props.modifyExpenseAllocation(this.state.id,changes);
      }
 
     render() {
 
         return (
-            <div className={"budget-category-item"}>
-                <Modal open={this.state.showEditModal} onClose={this.handleClose}>
+            <div className={"budget-category-item"} >
+                {(this.state.showEditModal)?
+                <Modal open={true} onClose={this.handleClose} size={"lg"}>
                     <Modal.Header>
                         <Modal.Title>{this.state.name}</Modal.Title>
                     </Modal.Header>
@@ -132,14 +116,13 @@ class BudgetCategoryItem extends React.Component {
                             <Form.Group controlId="amount">
                                 <Form.ControlLabel>Amount allocated</Form.ControlLabel>
                                 <Form.Control name="amount" accepter={InputNumber} defaultValue={this.state.amount} prefix={"$"} onBlur={async event => {
-                                    console.log(`Amount changed from ${this.state.amount} to ${event.target.value}`)
                                     this.setState({amount: event.target.value,});
                                     this.props.modifyBudgetItemAmount(this.state.id, event.target.value);
                                 }}/>
                                 <Form.HelpText tooltip>The amount the you are willing to allocated to the item</Form.HelpText>
                             </Form.Group>
                             <BudgetExpenseItemTransferList left={this.props.unallocatedExpenses}  right={(this.state.expenseList !== undefined) ? Object.entries(this.state.expenseList): []}
-                                                           handleExpenseAllocation={(changes)=> {this.handleExpenseListChange(changes); console.log(changes)}}/>
+                                                           handleExpenseAllocation={(changes)=> {this.handleExpenseListChange(changes);}} updateUnallocatedExpenses={(newList)=> this.handleExpenseListChange({left: newList, right: []})}/>
                         </Form>
                     </Modal.Body>
                     <Modal.Footer>
@@ -148,84 +131,99 @@ class BudgetCategoryItem extends React.Component {
                             Close
                         </Button>
                     </Modal.Footer>
-                </Modal>
+                </Modal>: null}
                 <div className={"budget-category-item-panel-grid"}>
                     <div className={"budget-category-item__name"}>
                         {(this.state.showNameInput)? <Input  style={{color: "#5692be"}}  defaultValue={this.state.name}  onBlur={async event => {
-                            this.props.handleOnNameChange(event.target.value, this.state.category, this.state.id);
-                            this.setState({name: event.target.value, showNameInput: false})
+                            if(this.state.name !==  event.target.value) {
+                                this.props.handleOnNameChange(event.target.value, this.state.category, this.state.id);
+                                this.setState({name: event.target.value, showNameInput: false})
+                            }
+                            else {
+                                this.setState({showNameInput: false})
+                            }
                         }}/> : <Input value={this.state.name}  readOnly onSelect={() => {this.setState({showNameInput: true})}}/>}
                     </div>
 
                     <div className={"budget-category-item__amount"} style={{borderRadius: 10}}  >
-                        <InputGroup>
-                            <InputGroup.Addon>$</InputGroup.Addon>
-                            <InputNumber defaultValue={this.state.amount} size="lg" onBlur={async event => {
-                                console.log(`Amount changed from ${this.state.amount} to ${event.target.value}`)
-                               this.setState({amount: event.target.value,});
-                                this.props.modifyBudgetItemAmount(this.state.id, event.target.value);
-                            }} />
-                        </InputGroup>
+                        {(this.state.showAmountInput)?
+                            <InputGroup inside>
+                                <InputGroup.Addon>$</InputGroup.Addon>
+                                <Input  style={{color: "#5692be"}}  defaultValue={this.state.amount}
+                                        onBlur={async event => {
+                                            if(this.state.amount !==  event.target.value) {
+                                                this.setState({amount: event.target.value, showAmountInput: false});
+                                                this.props.modifyBudgetItemAmount(this.state.id, event.target.value);
+                                            }
+                                            else {
+                                                this.setState({showAmountInput: false})
+                                            }
+                                            //this.props.modifyBudgetItemAmount(this.state.id, event.target.value);
+                                        }}/>
 
-
+                            </InputGroup>
+                            : <InputGroup inside>
+                                <InputGroup.Addon style={{color: "#656565"}}>$</InputGroup.Addon>
+                                <Input value={parseFloat(this.state.amount).toFixed(2)}  readOnly onSelect={() => {this.setState({showAmountInput: true})}}/>
+                            </InputGroup>}
                     </div>
                     <div className={"budget-category-item__info"} style={{background: "rgba(0,0,0,0.18)", borderRadius: 10}}>
-                        {(!this.props.draftMode)?
-                        <div className={"budget-category-item__info__feedback"} style={{float: "left"}}>
-                            {(this.state.item.amount - this.state.expensesAllocated < 0)?
-                                <Stack direction={"row"} spacing={5}>
-                                    <h6 style={{color: "#ff3535", fontWeight: "bold"}}>
-                                        {formatter.format(-1* (this.state.item.amount - this.state.expensesAllocated))}</h6>
-                                    <h6 style={{color: "#cecece"}}>Over Budget</h6>
-                                    <RemindFillIcon style={{color: "#ffc400"}}></RemindFillIcon>
-                                </Stack>
-                                :
-                                <Stack direction={"row"} spacing={5}>
-                                    <h6 style={{color: "#2f9b00"}}>{
-                                        //print the value of the difference between the amount allocated and the expenses allocated by going through expenseList and summing the values of amount
-                                        formatter.format(this.state.amount - this.state.expensesAllocated)
-                                    }</h6>
-                                    <h6 style={{color: "#696969"}}>Remaining</h6>
-                                </Stack> }
-                        </div>: null}
+
+                        <Whisper
+                            trigger="hover"
+                            controlId="control-id-hover"
+                            speaker={<Popover title={`${this.state.name}'s Allocated Expenses`}>
+                                <List dense={true}>
+                                    {(this.state.expenseList.length !== 0) ? Object.entries(this.state.expenseList).map((value) => {
+
+                                        return (<ListItem>
+                                            <ListItemAvatar>
+                                                <GiExpense style={{color: "#ff4000", fontSize: 30}}/>
+                                            </ListItemAvatar>
+                                            <ListItemText
+                                                secondary={
+                                                    <Stack>
+                                                        <h6 style={{color: "#787878"}}>{value[1].name}&nbsp;</h6>
+                                                        <h6 style={{color: "#ff0000"}}>{formatter.format(parseFloat(value[1].amount))}</h6>
+                                                    </Stack>}
+                                            />
+                                        </ListItem>)
+
+                                    }) :  <h6 style={{color: "#787878"}}>No allocated expenses</h6>
+                                    }
+                                </List>
+                            </Popover>}
+
+                            preventOverflow
+                            placement="auto"
+
+                        >
+
+                            <div className={"budget-category-item__info__feedback"} style={{float: "left"}}>
+                                {(this.state.item.amount - this.state.expensesAllocated < 0)?
+                                    <Stack direction={"row"} spacing={5}>
+                                        <h6 style={{color: "#ff3535", fontWeight: "bold"}}>
+                                            {formatter.format(-1* (this.state.item.amount - this.state.expensesAllocated))}</h6>
+                                        <h6 style={{color: "#cecece"}}>Over Budget</h6>
+                                        <RemindFillIcon style={{color: "#ffc400"}}></RemindFillIcon>
+                                        <h6 style={{color: "#ff3535"}}>({(this.state.expenseList !== undefined)? Object.keys(this.state.expenseList).length : 0})</h6>
+                                    </Stack>
+                                    :
+                                    <Stack direction={"row"} spacing={5}>
+                                        <h6 style={{color: "#2f9b00"}}>{
+                                            //print the value of the difference between the amount allocated and the expenses allocated by going through expenseList and summing the values of amount
+                                            formatter.format(this.state.amount - this.state.expensesAllocated)
+                                        }</h6>
+                                        <h6 style={{color: "#696969"}}>Remaining</h6>
+
+                                        {(Object.keys(this.state.expenseList).length !== 0)? <h6 style={{color: "#ff3535"}}>({Object.entries(this.state.expenseList).length})</h6> : null}
+                                    </Stack> }
+                            </div>
+                        </Whisper>
                         <Stack direction={"row"} spacing={10} alignItems={"flex-end"} justifyContent={"flex-end"} style={{float: "right"}}>
 
                             {(!this.props.draftMode)? <div>
-                            <Whisper
-                                trigger="hover"
-                                controlId="control-id-hover-enterable"
-                                speaker={<Popover title={`${this.state.name}'s Allocated Expenses`}>
-                                    <List dense={true}>
 
-                                        {(this.state.expenseList !== undefined) ? Object.entries(this.state.expenseList).map((value) => {
-                                            return (<ListItem>
-                                                <ListItemAvatar>
-                                                    <GiExpense style={{color: "#ff4000", fontSize: 30}}/>
-                                                </ListItemAvatar>
-                                                <ListItemText
-                                                    secondary={
-                                                    <Stack>
-                                                        <h6 style={{color: "#787878"}}>{value[1].name}&nbsp;</h6>
-                                                        <h6 style={{color: "#ff0000"}}>{formatter.format(value[1].amount)}</h6>
-                                                    </Stack>}
-                                                />
-                                                <IconButton aria-label="delete"
-                                                            style={{background: "rgba(0,0,0,0)", height: 30, width: 30}}
-                                                            size={"xs"}
-                                                            icon={<DeleteIcon className={"budget-category-item__info__delete-icon"}/>}/>
-                                            </ListItem>)
-
-                                        }) : "No linked expenses"}
-                                    </List>
-                                </Popover>}
-                                enterable
-                                preventOverflow
-                                placement="auto"
-
-                            >
-
-                                <Avatar className={"budget-category-item__info__expense-list-avatar"} style={{background: "#a80000"}} size={"sm"} >{(this.state.expenseList !== undefined)? Object.keys(this.state.expenseList).length : 0}</Avatar>
-                            </Whisper>
                                 <IconButton aria-label="edit"
                                             style={{
                                                 marginRight: 0,
@@ -243,7 +241,9 @@ class BudgetCategoryItem extends React.Component {
                                 <IconButton aria-label="delete"
                                             style={{background: "rgba(0,0,0,0)", height: 30, width: 30}}
                                             size={"xs"}
-                                            icon={<DeleteIcon className={"budget-category-item__info__delete-icon"}/>}/>
+                                            icon={<DeleteIcon className={"budget-category-item__info__delete-icon"}/>}
+                                            onClick={()=>this.props.handleRemoveBudgetItem(this.state.id)}
+                                />
 
                         </Stack>
                     </div>
