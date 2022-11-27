6399aec4936ae1deae4a7f763bd3755439d7ce6f
DineshB <dineshba03@gmail.com>
2022-11-26 20:07:58 -0600

    converted to smart feedback view

--- src/pages/main-tabs/Budget/BudgetCategoryRender.js
+++ src/pages/main-tabs/Budget/BudgetCategoryRender.js
@@ -3,9 +3,11 @@ import {
     IconButton, List, ListItem, ListItemAvatar, ListItemText,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import {LockOpen, LockSharp} from "@mui/icons-material";
+import {HelpSharp, LockOpen, LockSharp, QuestionMark} from "@mui/icons-material";
 import {GiExpense} from "react-icons/gi";
 import {formatter} from "./BudgetFormatters"
+import {Message, Popover, Stack, Whisper} from "rsuite";
+import {GrCircleQuestion} from "react-icons/gr";
 
 /** Renders and controls the panel that displays more specific information for each specific subcategory for the budget or income category.
  *  For budget categories, the panel shows that amount allocated and the expenses that linked to that subcategory
@@ -18,32 +20,22 @@ function BudgetCategoryRender({selectedCategory}) {
                 <IconButton size={"small"} onClick={() => setLock(!lock)}>
                     {(lock) ? <LockSharp scale={0.5}/> : <LockOpen/>}
                 </IconButton>
-                <p>You've selected</p>
-                <h3>{(selectedCategory === "None") ? "Nothing yet" : selectedCategory.name}</h3>
-                <p className={"moneyText"}>{(selectedCategory === "None") ? null : formatter.format(selectedCategory.amount)}</p>
-                {(!lock) ? <p className={"hoverText"}>Hover to view linked expenses</p> : null}
-                <p className={"linkedExpenseText"} style={(lock) ? {fontSize: 15} : null}>Linked Expenses:</p>
-                <List dense={true}>
-
-                    {(selectedCategory.expenseList !== undefined) ? Object.entries(selectedCategory.expenseList).map(value => {
-                        return (<ListItem
-                                secondaryAction={<IconButton edge="end" aria-label="delete">
-                                    <DeleteIcon/>
-                                </IconButton>}
-                            >
-                                <ListItemAvatar>
-
-                                    <GiExpense style={{color: "#ff4000", fontSize: 30}}/>
-
-                                </ListItemAvatar>
-                                <ListItemText
-                                    primary={value[1].name}
-                                    secondary={'Secondary text'}
-                                />
-                            </ListItem>)
-
-                    }) : "No linked expenses"}
-                </List>
+                <Whisper placement={ "top" } speaker={
+                    <Popover title={ "About Smart Feedback" }>
+                        <p>
+                            Smart Feedback is a tool that helps you to understand how your budget is allocated and how you can improve your budgeting to match practical spending patterns.
+                        </p>
+                    </Popover>
+                }>
+                    <Stack direction={"row"} justifyContent={"center"} spacing={10}>
+                        <h4 style={{color: "#5692be"}}>Smart Feedback</h4>
+                        <HelpSharp style={{marginTop: 10}}/>
+                    </Stack>
+                </Whisper>
+                <div>
+                    <Message  style={{marginRight: 15, marginLeft: 10, marginTop: 10, borderRadius: 20}} closable showIcon type="info" header="Coming Soon!">
+                        Soon, you will be able to see how your budget is allocated and how you can improve your budgeting to match practical spending patterns.                    </Message>
+                </div>
             </div>
         </div>)
 
