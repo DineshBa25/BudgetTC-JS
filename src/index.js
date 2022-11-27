e0f3f10f1f19ce989bdbfea3a767325ed3c8e3ed
DineshB <dineshba03@gmail.com>
2022-11-26 20:10:28 -0600

    add redux-persist so that the site can reload with data saved.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "rsuite/dist/rsuite.min.css";
-
-import { store} from "./redux/store";
+import {PersistGate} from 'redux-persist/integration/react'
+import { store, persistor} from "./redux/store";
 
 import { Provider } from "react-redux";
-
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
-            <App />
+          <PersistGate  loading={null} persistor={persistor}>
+                    <App />
+          </PersistGate>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
