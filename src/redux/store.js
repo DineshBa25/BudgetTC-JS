29405294dec7ee3677f6b1165b5a19633861e55b
DineshB <dineshba03@gmail.com>
2022-11-26 20:14:41 -0600

    modified to accommodate redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,35 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 
 import authReducer from "./slice/authSlice";
 import themeReducer from "./slice/themeSlice"
 import userDataReducer from "../pages/userDataSlice"
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+
+//combine auth them and userdata reducers
+const persistedReducer = persistReducer(persistConfig,
+    combineReducers({
+        auth: authReducer,
+        theme: themeReducer,
+        userData: userDataReducer
+    }));
+
 
 
 export const store = configureStore({
-    reducer:  {
-        auth: authReducer,
-       theme: themeReducer,
-        userData: userDataReducer
-    },
-
-
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk]
 })
+
+export const persistor = persistStore(store)
