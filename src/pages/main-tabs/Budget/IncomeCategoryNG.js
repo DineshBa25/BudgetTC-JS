f1afe8e79feee59ccd05ccf024a3e8c2a135e938
DineshB <dineshba03@gmail.com>
2023-02-25 17:58:33 -0600

    added the option to add an income categories and items

--- src/pages/main-tabs/Budget/IncomeCategoryNG.js
+++ src/pages/main-tabs/Budget/IncomeCategoryNG.js
@@ -5,6 +5,9 @@ import {Stack} from "rsuite";
 import {formatter} from "./BudgetFormatters";
 import IncomeCategoryItem from "./IncomeCategoryItem";
 import CategoryName from "./CategoryName";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 class IncomeCategoryNG extends React.Component {
         constructor() {
@@ -138,6 +141,98 @@ class IncomeCategoryNG extends React.Component {
         this.setState({expenseList: changes});
     }
 
+    handleAddIncomeItem(type, category) {
+        console.log("Add income item");
+        let newId = "item" + (this.state.oneTimeIncome.length + this.state.incomeCategories.length + 1);
+        if(type === "category"){
+            console.log("Adding item to income category");
+            update(ref(database), {
+                [this.props.address + category + "/items/" + newId]: {
+                    id: newId,
+                    name: "New item",
+                    amount: 0,
+                }
+            }).then(() => {
+                console.log("SUCCESS: added new item to income category in RTDB at " + this.props.address + category + "/items/" + newId);
+                //update state
+                this.setState({
+                    incomeCategories: this.state.incomeCategories.map((item) => {
+                        if(item.id === category){
+                            item.items.push({
+                                id: newId,
+                                name: "New item",
+                                amount: 0,
+                            })
+                        }
+                        return item;
+                    })
+                })
+
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+        else if(type === "oneTime"){
+            update(ref(database), {
+                [this.props.address + newId]: {
+                    id: newId,
+                    name: "New One Time Income",
+                    type: "oneTime",
+                    amount: 0,
+                }
+            }).then(() => {
+                console.log("SUCCESS: added new one time income to RTDB at " + this.props.address + newId);
+                //update state
+                this.setState({
+                    oneTimeIncome: [...this.state.oneTimeIncome, {
+                        id: newId,
+                        name: "New One Time Income",
+                        type: "oneTime",
+                        amount: 0,
+                    }]
+                })
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+    }
+
+    handleAddIncomeCategory() {
+        console.log("Add income category");
+        let newId = "item" + (this.state.categories.length + this.state.oneTimeIncome + 1);
+        update(ref(database), {
+            [this.props.address + newId]: {
+                id: newId,
+                name: "New Income Category",
+                type: "category",
+                items: {
+                    item1: {
+                        id: "item1",
+                        name: "New item",
+                        amount: 0,
+                    }
+                }
+            }
+        }).then(() => {
+            console.log("SUCCESS: added new income category to RTDB at " + this.props.address + newId);
+            //update state
+            this.setState({
+                incomeCategories: [...this.state.incomeCategories, {
+                    id: newId,
+                    name: "New Income Category",
+                    type: "category",
+                    items: [{
+                        id: "item1",
+                        name: "New item",
+                        amount: 0,
+                    }]
+                }]
+            })
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
     handleIncomeCategoryNameChange = (id, name) => {
         console.log("Name change: " + id + " " + name);
         //print address
@@ -181,36 +276,54 @@ class IncomeCategoryNG extends React.Component {
                                 }
 
                             </div>
+                            <div className={"budget-category-modification-buttongroup"}>
+                                <Button variant={"text"} startIcon={<AddIcon />} onClick={()=> this.handleAddIncomeItem("oneTime")}>Add One Time Income</Button>
+                            </div>
 
                             {this.state.incomeCategories.map((category) => {
                                 return <div>
-                                    <hr/>
-                            <div className={"budget-income-category-header"}>
-                                <Stack direction={"row"} spacing={5} >
-                                    <CategoryName name={category.name} id={category.id} handleNameChange={(name)=>this.handleIncomeCategoryNameChange(category.id,name)}/>
-                                    <div className={"budget-category-amount"} style={{color: "#1c9c02"}}>
-                                        <h4 >
-                                            {formatter.format(this.state.incomeCategories.find((item) => {
-                                                return item.id === category.id
-                                            }).items.reduce ((accumulator, currentValue) => {
-                                                return accumulator + currentValue.amount
-                                            }, 0))}
-                                        </h4>
-                                    </div>
-                                </Stack>
+                                            <hr/>
+                                            <div className={"budget-income-category-header"}>
+                                                <Stack direction={"row"} spacing={5} >
+                                                    <CategoryName name={category.name} id={category.id} handleNameChange={(name)=>this.handleIncomeCategoryNameChange(category.id,name)}/>
+                                                    <div className={"budget-category-amount"} style={{color: "#1c9c02"}}>
+                                                        <h4 >
+                                                            {formatter.format(this.state.incomeCategories.find((item) => {
+                                                                return item.id === category.id
+                                                            }).items.reduce ((accumulator, currentValue) => {
+                                                                return accumulator + currentValue.amount
+                                                            }, 0))}
+                                                        </h4>
+                                                    </div>
+                                                </Stack>
 
-                            </div>
-                            <div className={"budget-category-item-panel"}>
-                                    { (category.items !== undefined) ? category.items.map((category1) => {
-                                        return <IncomeCategoryItem item={category1}
-                                                                   handleNameChange={(id, name)=> this.handleNameChange(id,name, category.id)}
-                                                                   handleAmountChange={(id, amount)=> this.handleAmountChange(id,category.id,amount)}
-                                        />
+                                            </div>
+                                            <div className={"budget-category-item-panel"}>
+                                            { (category.items !== undefined) ? category.items.map((category1) => {
+                                                return <IncomeCategoryItem item={category1}
+                                                                           handleNameChange={(id, name)=> this.handleNameChange(id,name, category.id)}
+                                                                           handleAmountChange={(id, amount)=> this.handleAmountChange(id,category.id,amount)}
+                                                />
 
-                                    }): null}
+                                            }): null}
+                                            </div>
+                                            <div>
+                                                {(category.type !== "oneTime")?
+                                                    <div className={"budget-category-modification-buttongroup"}>
+                                                        <Stack direction={"row"} spacing={20}>
+                                                            <Button variant={"text"} startIcon={<AddIcon />} onClick={()=>{
+                                                                this.handleAddIncomeItem("category", category.id)
+                                                            }}>Add Income Item</Button>
 
-                            </div>
-                                </div>
+                                                            <Button variant="text" color="error" endIcon={<DeleteForeverIcon />} onClick={() => {
+                                                                console.log("delete category")
+                                                            }}>
+                                                                Delete Category
+                                                            </Button>
+                                                        </Stack>
+                                                    </div>:null }
+                                            </div>
+                                        </div>
                             })}
                         </div>
                     }
